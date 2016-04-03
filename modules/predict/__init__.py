8435a1cd957e133c8bd837d2bfd0ea526a891c20
am-shashank <am.seshank@gmail.com>
2016-04-02 20:22:29 -0400

    code for creating confusion matrix

--- modules/predict/__init__.py
+++ modules/predict/__init__.py
@@ -1,5 +1,7 @@
 from srilm import srilm_ppl
 from operator import itemgetter
+import pickle
+from sklearn.metric import confusion_matrix
 
 def lm_predict(models, test_file, pred_file, top_labels=1):
     with open(pred_file, 'w') as pred:
@@ -26,6 +28,44 @@ def lm_predict(models, test_file, pred_file, top_labels=1):
                         pred.write(",")
                 pred.write("\n")
 
+def lm_predict_xml(models, pickle_file, pred_file, top_labels=1):
+    # labelPredictions = []
+    # for i in range(top_labels):
+    #	labelPredictions.append([])
+    with open(pred_file, 'w') as pred:
+        with open(test_file, 'r') as test:
+	    abstract = pickle.load(test)
+	    allAbstractText = abstract.findall("AbstractText") 
+            for abstractText in allAbstractText:
+		line = abstractText.text
+		actualLabel = None
+`		if "Label" in abstractText.attrib:
+			actualLabel = abstractText.attrib["Label"]
+                print "PREDICTING FOR LINE:"
+                print line
+                ppls = []
+                for label, modelFile in models:
+                    print label, modelFile
+                    try:
+                        ppl = srilm_ppl(modelFile, line)
+                    except Exception as e:
+                        print "error in calculating ppl"
+                        ppl = 1000000
+                    print ppl
+                    ppls.append((label, ppl))
+                ppls.sort(key=lambda tup: tup[1])
+                # bestLabel = min(ppls, key=itemgetter(1))[0]
+                for i in range(0, top_labels):
+		    if actualLabel is not None:
+		    	pred.write(actualLabel + ":")
+		    # labelPredictions[i].append(ppls[i][0])
+                    pred.write(ppls[i][0])
+                    print "predicted: ", ppls[i][0]
+                    if i < top_labels - 1:
+                        pred.write(",")
+                pred.write("\n")
+
+
 def lm_predict_string(models, s):
     ppls = []
     for label, modelFile in models:
@@ -33,3 +73,27 @@ def lm_predict_string(models, s):
         ppls.append((label, ppl))
     bestLabel = min(ppls, key=itemgetter(1))[0]
     return (bestLabel, ppls)
+
+def lm_predict_full_text(model_file, test_file, pred_file, top_labels=1):
+    ppls = []
+    paragraphs = []
+    with open(pred_file, 'w') as pred:
+        with open(test_file, 'r') as test:
+            for line in test:
+		paragraphs.append(line)
+		try:
+                    ppl = srilm_ppl(model_file, line)
+                except Exception as e:
+                    print "error in calculating ppl"
+                    ppl = 1000000
+		ppls.append(ppl)
+	sorted_idx = [i[0] for i in sorted(enumerate(ppls), key=lambda x:x[1])] # sorted list of ppls
+	for i in range(top_labels):
+	    pred.write(paragraphs[sorted_idx[i]])
+	    pred.write("\n")
+		
+	
+	
+		
+		
+		
